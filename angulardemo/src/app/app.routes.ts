c294358bb1e8f2443fbc938f89ed4fd8607a87e7
huulooc <locnh22406@st.uel.edu.vn>
2024-11-10 14:00:50 +0700

    hoàn thiện detail

--- angulardemo/src/app/app.routes.ts
+++ angulardemo/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     {path: "home",component:HomeComponent},
     {path: "login",component:LoginComponent},
 
-    {path: "signup",component:SignupComponent}
+    {path: "signup",component:SignupComponent},
 
     {path: "home/:id",component:DetailComponent},
 
