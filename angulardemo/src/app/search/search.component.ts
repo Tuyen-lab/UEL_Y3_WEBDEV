49145028416434a8d96300addea0af10f16690f0
HuynhChau179 <chauhn22406@st.uel.edu.vn>
2024-11-14 10:18:16 +0700

    Sửa nav + thêm header

--- angulardemo/src/app/search/search.component.ts
+++ angulardemo/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent{
+  
+
+ 
+  
 
 }
